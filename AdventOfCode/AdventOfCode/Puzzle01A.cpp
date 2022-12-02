e5326011b6f74bde422dc2bcf4667d5899cd0423
Marcus Reid <(none)>
2022-12-01 17:45:03 -0800

    Tweak whitespace in Puzzle01A.

--- AdventOfCode/AdventOfCode/Puzzle01A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle01A.cpp
@@ -11,10 +11,10 @@ namespace Puzzle01A
 		// Add up the snack calories for each elf and find the highest total calories.
 		constexpr auto sumSnackCaloriesForElf =
 			[](const auto& currentElfInput)
-		{
-			auto elfSnackCalories = currentElfInput | std::views::transform([](const std::string& line) { return std::stoi(line); });
-			return std::accumulate(elfSnackCalories.begin(), elfSnackCalories.end(), 0);
-		};
+			{
+				auto elfSnackCalories = currentElfInput | std::views::transform([](const std::string& line) { return std::stoi(line); });
+				return std::accumulate(elfSnackCalories.begin(), elfSnackCalories.end(), 0);
+			};
 		auto caloriesPerElfView = input | std::views::split("") | std::views::transform(sumSnackCaloriesForElf);
 		auto maxCalories = std::ranges::max(caloriesPerElfView);
 
