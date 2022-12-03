12ae2ea76b79d82d72a0c08c7d2f901cdc0b1911
Marcus Reid <(none)>
2022-12-03 05:47:36 +0000

    Solve day 3.

--- AdventOfCode/AdventOfCode/Puzzle03A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle03A.cpp
@@ -8,6 +8,29 @@ namespace Puzzle03A
     {
         auto input = Utilities::ReadAllLinesInFile("Puzzle03.input");
 
-        std::cout << "Puzzle03A not yet solved!";
+        auto totalScore = 0;
+        for (const auto& line : input)
+        {
+            auto halfLineSize = line.size() / 2;
+            auto left = line.substr(0, halfLineSize);
+            auto right = line.substr(halfLineSize, halfLineSize);
+
+            std::ranges::sort(left);
+            std::ranges::sort(right);
+
+            auto common = std::string{};
+            std::ranges::set_intersection(left, right, std::back_inserter(common));
+
+            const auto [eraseFirst, eraseLast] = std::ranges::unique(common);
+            common.erase(eraseFirst, eraseLast);
+
+            totalScore += std::accumulate(common.begin(), common.end(), 0, 
+                [](int sum, char c)
+                {
+                    return sum + ('a' <= c && c <= 'z' ? 1 + c - 'a' : 27 + c - 'A' );
+                });
+        }
+
+        std::cout << totalScore;
     }
 }
--- AdventOfCode/AdventOfCode/Puzzle03B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle03B.cpp
@@ -6,8 +6,35 @@ namespace Puzzle03B
 {
     void PrintSolution()
     {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle03.input");
+		auto input = Utilities::ReadAllLinesInFile("Puzzle03.input");
 
-        std::cout << "Puzzle03B not yet solved!";
+		auto totalScore = 0;
+		for (auto i = 0; i < input.size(); i += 3)
+		{
+			auto elfA = input[i];
+			auto elfB = input[i + 1];
+			auto elfC = input[i + 2];
+
+			std::ranges::sort(elfA);
+			std::ranges::sort(elfB);
+			std::ranges::sort(elfC);
+
+			auto commonBetweenAB = std::string{};
+			std::ranges::set_intersection(elfA, elfB, std::back_inserter(commonBetweenAB));
+
+			auto commonBetweenABC = std::string{};
+			std::ranges::set_intersection(commonBetweenAB, elfC, std::back_inserter(commonBetweenABC));
+
+			const auto [eraseFirst, eraseLast] = std::ranges::unique(commonBetweenABC);
+			commonBetweenABC.erase(eraseFirst, eraseLast);
+
+			totalScore += std::accumulate(commonBetweenABC.begin(), commonBetweenABC.end(), 0,
+				[](int sum, char c)
+				{
+					return sum + ('a' <= c && c <= 'z' ? 1 + c - 'a' : 27 + c - 'A');
+				});
+		}
+
+		std::cout << totalScore;
     }
 }
