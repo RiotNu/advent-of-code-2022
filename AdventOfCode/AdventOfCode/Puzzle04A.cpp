7eea2d77537454cb039e2e1742531bf2adb5f67e
Marcus Reid <(none)>
2022-12-04 05:20:35 +0000

    Solve day 4.

--- AdventOfCode/AdventOfCode/Puzzle04A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle04A.cpp
@@ -8,6 +8,21 @@ namespace Puzzle04A
     {
         auto input = Utilities::ReadAllLinesInFile("Puzzle04.input");
 
-        std::cout << "Puzzle04A not yet solved!";
+        auto fullyContained = 0;
+        for (const auto& line : input)
+        {
+            auto elves = Utilities::SplitString(line, ",");
+
+            constexpr auto transform = [](const std::string& str) { return std::stoi(str); };
+            auto left = Utilities::SplitStringAndTransform<int>(elves[0], "-", transform);
+            auto right = Utilities::SplitStringAndTransform<int>(elves[1], "-", transform);
+
+            if (left[0] <= right[0] && right[1] <= left[1] || right[0] <= left[0] && left[1] <= right[1])
+            {
+                ++fullyContained;
+            }
+        }
+
+        std::cout << fullyContained;
     }
 }
--- AdventOfCode/AdventOfCode/Puzzle04B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle04B.cpp
@@ -4,10 +4,25 @@
 
 namespace Puzzle04B
 {
-    void PrintSolution()
-    {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle04.input");
+	void PrintSolution()
+	{
+		auto input = Utilities::ReadAllLinesInFile("Puzzle04.input");
 
-        std::cout << "Puzzle04B not yet solved!";
-    }
+		auto partiallyContained = 0;
+		for (const auto& line : input)
+		{
+			auto elves = Utilities::SplitString(line, ",");
+
+			constexpr auto transform = [](const std::string& str) { return std::stoi(str); };
+			auto left = Utilities::SplitStringAndTransform<int>(elves[0], "-", transform);
+			auto right = Utilities::SplitStringAndTransform<int>(elves[1], "-", transform);
+
+			if (left[0] <= right[0] && right[0] <= left[1] || right[0] <= left[0] && left[0] <= right[1])
+			{
+				++partiallyContained;
+			}
+		}
+
+		std::cout << partiallyContained;
+	}
 }
