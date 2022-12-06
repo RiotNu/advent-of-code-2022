bb165455f2f5f7d42d9731065e7edba4be00c739
Marcus Reid <(none)>
2022-12-06 05:32:16 +0000

    Tidy up puzzle 6.

--- AdventOfCode/AdventOfCode/Puzzle06A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle06A.cpp
@@ -9,21 +9,23 @@ namespace Puzzle06A
         auto input = Utilities::ReadAllLinesInFile("Puzzle06.input");
         const auto& line = input[0];
 
-        constexpr int desiredPacketSize = 4;
-        auto packetEndPosition = -1;
-        for (auto i = 0; i < line.size() - desiredPacketSize; ++i)
+        constexpr int desiredHeaderSize = 4;
+        auto headerEndIndex = -1;
+        for (auto i = 0; i < line.size() - desiredHeaderSize; ++i)
         {
-            auto packet = line.substr(i, desiredPacketSize);
-            std::ranges::sort(packet);
-            auto [first, last] = std::ranges::unique(packet);
-            packet.erase(first, last);
-            if (packet.size() == desiredPacketSize)
+            auto header = line.substr(i, desiredHeaderSize);
+            std::ranges::sort(header);
+            
+            auto [first, last] = std::ranges::unique(header);
+            header.erase(first, last);
+            
+            if (header.size() == desiredHeaderSize)
             {
-                packetEndPosition = i + desiredPacketSize;
+                headerEndIndex = i + desiredHeaderSize;
                 break;
             }
         }
 
-        std::cout << packetEndPosition;
+        std::cout << headerEndIndex;
     }
 }
