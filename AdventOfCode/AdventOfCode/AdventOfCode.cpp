9e057e591fcd306147dafa32c252e5851eb57485
Marcus Reid <(none)>
2022-12-06 04:06:36 +0000

    Update for day 6.

--- AdventOfCode/AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode/AdventOfCode.cpp
@@ -7,8 +7,8 @@
 
 int main()
 {
-	constexpr auto partASolver = &Puzzle05A::PrintSolution;
-	constexpr auto partBSolver = &Puzzle05B::PrintSolution;
+	constexpr auto partASolver = &Puzzle06A::PrintSolution;
+	constexpr auto partBSolver = &Puzzle06B::PrintSolution;
 
 	for (auto i = 0; i < 2; ++i)
 	{
