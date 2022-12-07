be8fcb683a9c3c6f5ffda20a0262a840e19c60aa
Marcus Reid <(none)>
2022-12-07 17:01:26 +0000

    Solve day 7.

--- AdventOfCode/AdventOfCode/Puzzle07A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle07A.cpp
@@ -4,10 +4,111 @@
 
 namespace Puzzle07A
 {
-    void PrintSolution()
-    {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle07.input");
+	struct File
+	{
+		std::string path;
+		int size = 0;
+	};
 
-        std::cout << "Puzzle07A not yet solved!";
-    }
+	struct Directory
+	{
+		std::string path;
+		int size = -1;
+		std::unordered_set<std::string> childDirectories;
+		std::unordered_set<std::string> childFiles;
+	};
+
+	auto allDirectories = std::unordered_map<std::string, Directory>{};
+	auto allFiles = std::unordered_map<std::string, File>{};
+
+	void ReadInput()
+	{
+		auto input = Utilities::ReadAllLinesInFile("Puzzle07.input");
+
+		allDirectories["/"] = Directory{ "/" };
+		auto pwd = std::vector<std::string>{ {"/"} };
+
+		for (const auto& line : input)
+		{
+			auto tokens = Utilities::SplitString(line, " ");
+			if (tokens[0] == "$")
+			{
+				if (tokens[1] == "ls")
+				{
+					continue;
+				}
+				else
+				{
+					Utilities::VerifyElseCrash(tokens[1] == "cd");
+					const auto& target = tokens[2];
+					if (target == "/")
+					{
+						pwd.clear();
+						pwd.emplace_back("/");
+					}
+					else if (target == "..")
+					{
+						pwd.pop_back();
+					}
+					else
+					{
+						pwd.emplace_back(pwd.back() + target + "/");
+					}
+				}
+			}
+			else
+			{
+				auto& parent = allDirectories.at(pwd.back());
+				if (tokens[0] == "dir")
+				{
+					auto path = pwd.back() + tokens[1] + "/";
+					if (!allDirectories.contains(path))
+					{
+						allDirectories[path] = Directory{ path };
+					}
+
+					parent.childDirectories.insert(path);
+				}
+				else
+				{
+					auto path = pwd.back() + tokens[1];
+					if (!allFiles.contains(path))
+					{
+						allFiles[path] = File{ path, std::stoi(tokens[0]) };
+					}
+
+					parent.childFiles.insert(path);
+				}
+			}
+		}
+	}
+
+	int CalculateDirectorySize(Directory& directory)
+	{
+		if (directory.size >= 0)
+		{
+			return directory.size;
+		}
+
+		auto totalSizeOfFiles = ranges::accumulate(directory.childFiles, 0,
+			[](int sum, const std::string& path) { return sum + allFiles.at(path).size; });
+
+		directory.size = ranges::accumulate(directory.childDirectories, totalSizeOfFiles,
+			[](int sum, const std::string& path) { return sum + CalculateDirectorySize(allDirectories.at(path)); });
+		return directory.size;
+	}
+
+	void PrintSolution()
+	{
+		ReadInput();
+		CalculateDirectorySize(allDirectories.at("/"));
+
+		auto isSmallDir = [](const Directory& directory){ return directory.size <= 100000; };
+		auto totalDirSize = ranges::accumulate(
+			allDirectories | std::ranges::views::values | std::ranges::views::filter(isSmallDir),
+			0,
+			[](int sum, const Directory& directory) { return sum + directory.size; });
+
+		std::cout << totalDirSize;
+	}
 }
--- AdventOfCode/AdventOfCode/Puzzle07B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle07B.cpp
@@ -4,10 +4,123 @@
 
 namespace Puzzle07B
 {
-    void PrintSolution()
-    {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle07.input");
+	struct File
+	{
+		std::string path;
+		int size = 0;
+	};
 
-        std::cout << "Puzzle07B not yet solved!";
-    }
+	struct Directory
+	{
+		std::string path;
+		int size = -1;
+		std::unordered_set<std::string> childDirectories;
+		std::unordered_set<std::string> childFiles;
+	};
+
+	auto allDirectories = std::unordered_map<std::string, Directory>{};
+	auto allFiles = std::unordered_map<std::string, File>{};
+
+	void ReadInput()
+	{
+		auto input = Utilities::ReadAllLinesInFile("Puzzle07.input");
+
+		allDirectories["/"] = Directory{ "/" };
+		auto pwd = std::vector<std::string>{ {"/"} };
+
+		for (const auto& line : input)
+		{
+			auto tokens = Utilities::SplitString(line, " ");
+			if (tokens[0] == "$")
+			{
+				if (tokens[1] == "ls")
+				{
+					continue;
+				}
+				else
+				{
+					Utilities::VerifyElseCrash(tokens[1] == "cd");
+					const auto& target = tokens[2];
+					if (target == "/")
+					{
+						pwd.clear();
+						pwd.emplace_back("/");
+					}
+					else if (target == "..")
+					{
+						pwd.pop_back();
+					}
+					else
+					{
+						pwd.emplace_back(pwd.back() + target + "/");
+					}
+				}
+			}
+			else
+			{
+				auto& parent = allDirectories.at(pwd.back());
+				if (tokens[0] == "dir")
+				{
+					auto path = pwd.back() + tokens[1] + "/";
+					if (!allDirectories.contains(path))
+					{
+						allDirectories[path] = Directory{ path };
+					}
+
+					parent.childDirectories.insert(path);
+				}
+				else
+				{
+					auto path = pwd.back() + tokens[1];
+					if (!allFiles.contains(path))
+					{
+						allFiles[path] = File{ path, std::stoi(tokens[0]) };
+					}
+
+					parent.childFiles.insert(path);
+				}
+			}
+		}
+	}
+
+	int CalculateDirectorySize(Directory& directory)
+	{
+		if (directory.size >= 0)
+		{
+			return directory.size;
+		}
+
+		auto totalSizeOfFiles = ranges::accumulate(directory.childFiles, 0,
+			[](int sum, const std::string& path) { return sum + allFiles.at(path).size; });
+
+		directory.size = ranges::accumulate(directory.childDirectories, totalSizeOfFiles,
+			[](int sum, const std::string& path) { return sum + CalculateDirectorySize(allDirectories.at(path)); });
+		return directory.size;
+	}
+
+	void PrintSolution()
+	{
+		ReadInput();
+		CalculateDirectorySize(allDirectories.at("/"));
+
+		constexpr auto totalSpace = 70000000;
+		constexpr auto desiredSpace = 30000000;
+		const auto unusedSpace = totalSpace - allDirectories.at("/").size;
+		const auto sizeToDelete = desiredSpace - unusedSpace;
+
+		auto directoriesSortedBySize = allDirectories | std::ranges::views::values | std::ranges::to<std::vector>();
+		std::ranges::sort(directoriesSortedBySize, std::less<int>{}, [](const Directory& directory) { return directory.size; });
+
+		auto deletedDirectorySize = 0;
+		for (const auto& directory : directoriesSortedBySize)
+		{
+			if (directory.size >= sizeToDelete)
+			{
+				deletedDirectorySize = directory.size;
+				break;
+			}
+		}
+
+		std::cout << deletedDirectorySize;
+	}
 }
