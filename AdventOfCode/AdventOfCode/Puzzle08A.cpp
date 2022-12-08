c5a96b3f4298310b7868d5df8d461c20e01e6221
Marcus Reid <(none)>
2022-12-08 06:41:54 +0000

    Solve day 8.

--- AdventOfCode/AdventOfCode/Puzzle08A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle08A.cpp
@@ -4,10 +4,42 @@
 
 namespace Puzzle08A
 {
-    void PrintSolution()
-    {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle08.input");
+	void PrintSolution()
+	{
+		auto input = Utilities::ReadAllLinesInFile("Puzzle08.input");
 
-        std::cout << "Puzzle08A not yet solved!";
-    }
+		auto grid = Utilities::Grid2d<int>(input[0].size(), input.size());
+		for (auto y = 0; y < input.size(); ++y)
+		{
+			const auto& line = input[y];
+			for (auto x = 0; x < line.size(); ++x)
+			{
+				grid.at(x, y) = line[x] - '0';
+			}
+		}
+
+		auto countVisible = grid.Width() * 2 + (grid.Height() - 2) * 2;
+		for (auto y = 1; y < grid.Height() - 1; ++y)
+		{
+			for (auto x = 1; x < grid.Width() - 1; ++x)
+			{
+				auto current = grid.at(x, y);
+
+				const auto checkRow = [&grid, y, current](int i) { return grid.at(i, y) < current; };
+				auto visibleFromLeft = std::ranges::all_of(std::ranges::views::iota(0, x), checkRow);
+				auto visibleFromRight = std::ranges::all_of(std::ranges::views::iota(x + 1, static_cast<int>(grid.Width())), checkRow);
+
+				const auto checkColumn = [&grid, x, current](int i) { return grid.at(x, i) < current; };
+				auto visibleFromTop = std::ranges::all_of(std::ranges::views::iota(0, y), checkColumn);
+				auto visibleFromBottom = std::ranges::all_of(std::ranges::views::iota(y + 1, static_cast<int>(grid.Height())), checkColumn);
+
+				if (visibleFromLeft || visibleFromRight || visibleFromTop || visibleFromBottom)
+				{
+					++countVisible;
+				}
+			}
+		}
+
+		std::cout << countVisible;
+	}
 }
--- AdventOfCode/AdventOfCode/Puzzle08B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle08B.cpp
@@ -4,10 +4,59 @@
 
 namespace Puzzle08B
 {
-    void PrintSolution()
-    {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle08.input");
+	void PrintSolution()
+	{
+		auto input = Utilities::ReadAllLinesInFile("Puzzle08.input");
 
-        std::cout << "Puzzle08B not yet solved!";
-    }
+		auto grid = Utilities::Grid2d<int>(input[0].size(), input.size());
+		for (auto y = 0; y < input.size(); ++y)
+		{
+			const auto& line = input[y];
+			for (auto x = 0; x < line.size(); ++x)
+			{
+				grid.at(x, y) = line[x] - '0';
+			}
+		}
+
+		auto scenicScoresGrid = Utilities::Grid2d<int>(grid.Width(), grid.Height());
+		for (auto y = 1; y < grid.Height() - 1; ++y)
+		{
+			for (auto x = 1; x < grid.Width() - 1; ++x)
+			{
+				auto current = grid.at(x, y);
+
+				auto visibleLeft = 0;
+				for (int i = x - 1; i >= 0; --i)
+				{
+					++visibleLeft;
+					if (grid.at(i, y) >= current) { break; }
+				}
+
+				auto visibleRight = 0;
+				for (int i = x + 1; i < grid.Width(); ++i)
+				{
+					++visibleRight;
+					if (grid.at(i, y) >= current) { break; }
+				}
+
+				auto visibleTop = 0;
+				for (int i = y - 1; i >= 0; --i)
+				{
+					++visibleTop;
+					if (grid.at(x, i) >= current) { break; }
+				}
+
+				auto visibleBottom = 0;
+				for (int i = y + 1; i < grid.Height(); ++i)
+				{
+					++visibleBottom;
+					if (grid.at(x, i) >= current) { break; }
+				}
+
+				scenicScoresGrid.at(x, y) = visibleLeft * visibleRight * visibleTop * visibleBottom;
+			}
+		}
+
+		std::cout << std::ranges::max(scenicScoresGrid);
+	}
 }
