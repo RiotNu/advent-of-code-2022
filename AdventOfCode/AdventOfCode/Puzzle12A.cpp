de0ae75be74ccf719447098a0bb425846f16d39f
Marcus Reid <(none)>
2022-12-12 17:21:59 +0000

    Solve day 12.

--- AdventOfCode/AdventOfCode/Puzzle12A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle12A.cpp
@@ -2,12 +2,86 @@
 #include "../Utilities/Utilities.h"
 #include "PuzzleSolvers.h"
 
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
 namespace Puzzle12A
 {
-    void PrintSolution()
-    {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle12.input");
+	using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property, boost::property<boost::edge_weight_t, int>>;
 
-        std::cout << "Puzzle12A not yet solved!";
-    }
+	bool HasEdge(const Utilities::Grid2d<char>& grid, int x1, int y1, int x2, int y2)
+	{
+		if (x2 < 0 || x2 >= grid.Width() || y2 < 0 || y2 >= grid.Height())
+		{
+			return false;
+		}
+
+		return grid.at(x2, y2) <= grid.at(x1, y1) + 1;
+	}
+
+	void PrintSolution()
+	{
+		auto input = Utilities::ReadAllLinesInFile("Puzzle12.input");
+		auto grid = Utilities::Grid2d<char>(input[0].size(), input.size());
+		auto start = 0ull;
+		auto end = 0ull;
+		for (auto y = 0; y < input.size(); ++y)
+		{
+			const auto& line = input[y];
+			for (auto x = 0; x < line.size(); ++x)
+			{
+				auto val = line[x];
+				if (val == 'S')
+				{
+					start = grid.GetIndexFromCoordinates(x, y);
+					val = 'a';
+				}
+				else if (val == 'E')
+				{
+					end = grid.GetIndexFromCoordinates(x, y);
+					val = 'z';
+				}
+				grid.at(x, y) = val;
+			}
+		}
+
+		constexpr auto weight = 1;
+		auto graph = Graph{};
+		for (auto y = 0; y < grid.Height(); ++y)
+		{
+			for (auto x = 0; x < grid.Width(); ++x)
+			{
+				auto source = grid.GetIndexFromCoordinates(x, y);
+				if (HasEdge(grid, x, y, x - 1, y))
+				{
+					auto target = grid.GetIndexFromCoordinates(x - 1, y);
+					boost::add_edge(source, target, weight, graph);
+				}
+
+				if (HasEdge(grid, x, y, x + 1, y))
+				{
+					auto target = grid.GetIndexFromCoordinates(x + 1, y);
+					boost::add_edge(source, target, weight, graph);
+				}
+
+				if (HasEdge(grid, x, y, x, y - 1))
+				{
+					auto target = grid.GetIndexFromCoordinates(x, y - 1);
+					boost::add_edge(source, target, weight, graph);
+				}
+
+				if (HasEdge(grid, x, y, x, y + 1))
+				{
+					auto target = grid.GetIndexFromCoordinates(x, y + 1);
+					boost::add_edge(source, target, weight, graph);
+				}
+			}
+		}
+
+		auto predecessors = std::vector<Graph::vertex_descriptor>(boost::num_vertices(graph));
+		auto distances = std::vector<int>(boost::num_vertices(graph));
+		boost::dijkstra_shortest_paths(graph, start, boost::distance_map(distances.data()).predecessor_map(predecessors.data()));
+
+		std::cout << distances[end];
+	}
 }
--- AdventOfCode/AdventOfCode/Puzzle12B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle12B.cpp
@@ -2,12 +2,96 @@
 #include "../Utilities/Utilities.h"
 #include "PuzzleSolvers.h"
 
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
 namespace Puzzle12B
 {
-    void PrintSolution()
-    {
-        auto input = Utilities::ReadAllLinesInFile("Puzzle12.input");
+	using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property, boost::property<boost::edge_weight_t, int>>;
 
-        std::cout << "Puzzle12B not yet solved!";
-    }
+	bool HasEdge(const Utilities::Grid2d<char>& grid, int x1, int y1, int x2, int y2)
+	{
+		if (x2 < 0 || x2 >= grid.Width() || y2 < 0 || y2 >= grid.Height())
+		{
+			return false;
+		}
+
+		return grid.at(x2, y2) <= grid.at(x1, y1) + 1;
+	}
+
+	void PrintSolution()
+	{
+		auto input = Utilities::ReadAllLinesInFile("Puzzle12.input");
+		auto grid = Utilities::Grid2d<char>(input[0].size(), input.size());
+		auto start = 0ull;
+		for (auto y = 0; y < input.size(); ++y)
+		{
+			const auto& line = input[y];
+			for (auto x = 0; x < line.size(); ++x)
+			{
+				auto val = line[x];
+				if (val == 'S')
+				{
+					val = 'a';
+				}
+				else if (val == 'E')
+				{
+					start = grid.GetIndexFromCoordinates(x, y);
+					val = 'z';
+				}
+				grid.at(x, y) = val;
+			}
+		}
+
+		constexpr auto weight = 1;
+		auto graph = Graph{};
+		for (auto y = 0; y < grid.Height(); ++y)
+		{
+			for (auto x = 0; x < grid.Width(); ++x)
+			{
+				// Reverse all edges from part 1 by flipping source and target when adding edges.
+				auto source = grid.GetIndexFromCoordinates(x, y);
+				if (HasEdge(grid, x, y, x - 1, y))
+				{
+					auto target = grid.GetIndexFromCoordinates(x - 1, y);
+					boost::add_edge(target, source, weight, graph);
+				}
+
+				if (HasEdge(grid, x, y, x + 1, y))
+				{
+					auto target = grid.GetIndexFromCoordinates(x + 1, y);
+					boost::add_edge(target, source, weight, graph);
+				}
+
+				if (HasEdge(grid, x, y, x, y - 1))
+				{
+					auto target = grid.GetIndexFromCoordinates(x, y - 1);
+					boost::add_edge(target, source, weight, graph);
+				}
+
+				if (HasEdge(grid, x, y, x, y + 1))
+				{
+					auto target = grid.GetIndexFromCoordinates(x, y + 1);
+					boost::add_edge(target, source, weight, graph);
+				}
+			}
+		}
+
+		auto predecessors = std::vector<Graph::vertex_descriptor>(boost::num_vertices(graph));
+		auto distances = std::vector<int>(boost::num_vertices(graph));
+		boost::dijkstra_shortest_paths(graph, start, boost::distance_map(distances.data()).predecessor_map(predecessors.data()));
+
+		auto minDistance = static_cast<int>(grid.size());
+		for (auto i = 0; i < distances.size(); ++i)
+		{
+			if (grid.at(i) != 'a')
+			{
+				continue;
+			}
+
+			minDistance = std::min(minDistance, distances[i]);
+		}
+
+		std::cout << minDistance;
+	}
 }
